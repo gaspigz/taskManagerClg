c9c41110a9d45deb1f4418e7b66dc5778e621b02
Gaspar Giménez <gaspar.gimenez8@gmail.com>
2025-07-28 17:49:39 -0300

    Prisma failed conn, docker ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, TasksModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
