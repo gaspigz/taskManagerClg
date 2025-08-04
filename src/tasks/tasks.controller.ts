9bb0a2cbf7db96b753bb160747f6bfdd831f8d21
Gaspar Giménez <gaspar.gimenez8@gmail.com>
2025-08-04 15:05:03 -0300

    bug fix rollback

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,62 +1,76 @@
 import { Controller, Get, Post, Body, Query, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiOperation, ApiBody, ApiParam, ApiQuery } from '@nestjs/swagger';
+import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { TasksService } from './tasks.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { TaskOwnerGuard } from 'src/auth/guards/task-owner.guard';
+import { ApiBody, ApiOperation, ApiQuery } from '@nestjs/swagger';
 
-@ApiTags('Tasks')
-@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
-  @ApiOperation({ summary: 'Crear nueva tarea (User o Admin)' })
+
+  @ApiOperation({ summary: 'Create a new task' })
   @ApiBody({ type: CreateTaskDto })
+  @Post()
   create(@Body() createTaskDto: CreateTaskDto, @Request() req) {
     return this.tasksService.create(createTaskDto, req.user);
   }
 
-  @Get()
-  @ApiOperation({ summary: 'Listar tareas del usuario autenticado' })
+  @ApiOperation({ summary: 'Get all tasks with optional filters' })
   @ApiQuery({ name: 'title', required: false })
   @ApiQuery({ name: 'type', required: false, enum: ['URGENT', 'MEDIUM', 'LOW'] })
+  @ApiQuery({ name: 'sortBy', required: false, enum: ['createdAt', 'updatedAt', 'title'], example: 'createdAt' })
+  @ApiQuery({ name: 'order', required: false, enum: ['asc', 'desc'], example: 'desc' })
+  @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
+  @Get()
   findAll(@Request() req, @Query() query: any) {
+    console.log("Fetching all tasks for user:", req.user);
+
     return this.tasksService.findAll(req.user, query);
   }
 
+  @ApiOperation({ summary: 'Get a task by ID' })
+  @UseGuards(TaskOwnerGuard)
   @Get(':id')
-  @ApiOperation({ summary: 'Obtener una tarea por ID (Propietario o admin)' })
-  @ApiParam({ name: 'id', type: 'string' })
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id);
   }
 
-  @Patch(':id')
-  @ApiOperation({ summary: 'Actualizar tarea (Propietario o admin)' })
-  @ApiParam({ name: 'id', type: 'string' })
+  @ApiOperation({ summary: 'Update a task by ID' })
   @ApiBody({ type: UpdateTaskDto })
+  @UseGuards(TaskOwnerGuard)
+  @Patch(':id')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(+id, updateTaskDto);
   }
 
+  @ApiOperation({ summary: 'Archive a task by ID' })
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
   @Patch(':id/archive')
-  @ApiOperation({ summary: 'Archivar tarea (ADMIN)' })
-  @ApiParam({ name: 'id', type: 'string' })
   archive(@Param('id') id: string) {
     return this.tasksService.archive(+id);
   }
 
+  @ApiOperation({ summary: 'Get tasks from a specific user' })
+  @ApiQuery({ name: 'userId', required: true, type: Number })
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
   @Get('user/:userId')
-  @ApiOperation({ summary: 'Listar tareas de un usuario específico (solo ADMIN)' })
-  @ApiParam({ name: 'userId', type: 'string' })
   getTasksFromUser(@Param('userId') userId: string) {
     return this.tasksService.getTasksFromUser(+userId);
   }
 
+  @ApiOperation({ summary: 'Remove a task by ID' })
+
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
   @Delete(':id')
-  @ApiOperation({ summary: 'Eliminar tarea por ID (solo ADMIN)' })
-  @ApiParam({ name: 'id', type: 'string' })
   remove(@Param('id') id: string) {
     return this.tasksService.remove(+id);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,47 +1,75 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiOperation, ApiBody, ApiParam } from '@nestjs/swagger';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { OwnerGuard } from 'src/auth/guards/owner.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
-@ApiTags('Users')
-@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  @ApiOperation({ summary: 'Crear nuevo usuario (ADMIN)' })
+  @ApiOperation({ summary: 'Create a new user. Only available for admins.' })
   @ApiBody({ type: CreateUserDto })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
+  @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    console.log("Creating user with data:", createUserDto);
     return this.usersService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Get all users. Only available for admins.' })
+  @ApiBody({ type: CreateUserDto })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get()
-  @ApiOperation({ summary: 'Listar todos los usuarios (ADMIN)' })
   findAll() {
+    console.log("Fetching all users.");
     return this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get a user by ID. Only available for the user themselves or admins.' })
+  @ApiBody({ type: CreateUserDto })
+  @UseGuards(JwtAuthGuard, OwnerGuard)
   @Get(':id')
-  @ApiOperation({ summary: 'Obtener un usuario por ID (Propietario)' })
-  @ApiParam({ name: 'id', type: 'string' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
-  @ApiOperation({ summary: 'Actualizar usuario (Propietario)' })
-  @ApiParam({ name: 'id', type: 'string' })
+  @ApiOperation({ summary: 'Update a user by ID. Only available for the user themselves or admins.' })
   @ApiBody({ type: UpdateUserDto })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @UseGuards(JwtAuthGuard, OwnerGuard)
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    if (await this.usersService.update(+id, updateUserDto)) {
+      return { message: 'User updated successfully' };
+    } else {
+      return { message: 'User not found' };
+    }
   }
 
+  @ApiOperation({ summary: 'Delete a user by ID. Only available for admins.' })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Delete(':id')
-  @ApiOperation({ summary: 'Eliminar usuario (ADMIN)' })
-  @ApiParam({ name: 'id', type: 'string' })
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    console.log(`Removing user with ID: ${id}`);
+    if (isNaN(+id)) {
+      throw new HttpException('Invalid ID format', 400);
+    }
+    console.log(`Calling service to remove user with ID: ${id}`);
+    if (+id <= 0) {
+      throw new HttpException('ID must be a positive integer', 400);
+    }
+
+    if (await this.usersService.remove(+id)) {
+      return { message: 'User removed successfully' };
+    } else {
+      return { message: 'User not found' };
+    }
   }
 }
